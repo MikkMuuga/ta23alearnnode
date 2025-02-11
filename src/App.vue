3ce6983d333ed12bb5482f581ecfd0a9546ac776
Kaspar Martin Suursalu <kasparsu@gmail.com>
2025-02-11 10:51:29 +0200

    Simple routing pages components

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <script setup>
 import { ref } from 'vue';
 import Tabs from './components/Tabs.vue';
-let titles = ['Item1', 'Item2', 'Item3', 'Item4'];
-let contents = ['content 1', 'content 2', 'content 3', 'content 4'];
+
+import Modals from './pages/Modals.vue';
+import ToDo from './pages/ToDo.vue';
+
+let titles = ['ToDo', 'Modals'];
+let contents = [ToDo, Modals];
 let activeTab = ref(0);
 function change(key){
     console.log(key);
@@ -13,9 +17,7 @@ function change(key){
 
 <template>
     <Tabs :items="titles" @change="change"></Tabs>
-    <div class="container">
-        <h1>{{ contents[activeTab] }}</h1>
-    </div>
+    <component :is="contents[activeTab]"></component>
 </template>
 
 <style>
