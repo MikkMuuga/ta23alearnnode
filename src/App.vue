a5532342084d3b57287d68998786f32dc185cad0
Kaspar Martin Suursalu <kasparsu@gmail.com>
2025-02-11 10:30:16 +0200

    Simple content switching

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <script setup>
+import { ref } from 'vue';
 import Tabs from './components/Tabs.vue';
 let titles = ['Item1', 'Item2', 'Item3', 'Item4'];
+let contents = ['content 1', 'content 2', 'content 3', 'content 4'];
+let activeTab = ref(0);
+function change(key){
+    console.log(key);
+    activeTab.value = key;
+}
 
 </script>
 
 <template>
-    <Tabs :items="titles"></Tabs>
-
+    <Tabs :items="titles" @change="change"></Tabs>
+    <div class="container">
+        <h1>{{ contents[activeTab] }}</h1>
+    </div>
 </template>
 
 <style>
