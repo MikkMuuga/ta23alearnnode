2a0745b336bd33e48561a9f3aa1239a68d13ed66
Kaspar Martin Suursalu <kasparsu@gmail.com>
2025-02-18 10:42:49 +0200

    Add chuck norris example with xhr

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,11 @@ import Tabs from './components/Tabs.vue';
 
 <template>
     <Tabs></Tabs>
-    <RouterView></RouterView>
+    <div class="container">
+        <section class="section">
+            <RouterView></RouterView>
+        </section>
+    </div>
 </template>
 
 <style>
